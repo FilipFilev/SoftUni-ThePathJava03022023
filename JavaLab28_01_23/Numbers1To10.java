ba74f6b2e62e82fb4365cc18ff22a6e34a8347c1
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-03 14:42:43 +0200

    New codes and classes in this Repository. The Path of Java. Code Filip. :)

--- JavaLab28_01_23/Numbers1To10.java
+++ JavaLab28_01_23/Numbers1To10.java
@@ -1,5 +1,16 @@
 package JavaLab28_01_23;
 
 public class Numbers1To10 {
-    
+        public static void main(String[] args) {
+                System.out.println(1);
+                System.out.println(2);
+                System.out.println(3);
+                System.out.println(4);
+                System.out.println(5);
+                System.out.println(6);
+                System.out.println(7);
+                System.out.println(8);
+                System.out.println(9);
+                System.out.println(10);
+        }
 }
